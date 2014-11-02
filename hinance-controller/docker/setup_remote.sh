bbd70581f3694c8aa5d768bb6d2bbef46f19a84a
Oleg Plakhotniuk <olegus8@gmail.com>
2014-11-02 01:01:52 -0600

    fix

--- hinance-controller/docker/setup_remote.sh
+++ hinance-controller/docker/setup_remote.sh
@@ -9,6 +9,6 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install git docker.io
 
-sudo mkdir /etc/$APP
+sudo mkdir -p /etc/$APP
 sudo git clone -b "$APP_VERSION" \
     https://github.com/olegus8/hinance.git /usr/share/$APP/repo
