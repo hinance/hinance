025b7e985ec38ae0d5d9b2573927afcbd889247c
Oleg Plakhotniuk <olegus8@gmail.com>
2014-11-02 18:54:18 -0600

    my

--- hinance-worker/run.sh
+++ hinance-worker/run.sh
@@ -3,6 +3,7 @@
 set -e
 
 APP="hinance-worker"
+IMAGE="olegus8/$APP:$APP_VERSION"
 
 . /usr/share/$APP/repo/config.sh
 
@@ -16,14 +17,14 @@ if docker ps -a|grep $APP >/dev/null ; then
     docker rm $APP >/dev/null
 fi
 
-if ! docker run --rm $APP:$APP_VERSION pwd >/dev/null 2>&1 ; then
-    echo "Building $APP:$APP_VERSION container."
-    docker build --rm -t $APP:$APP_VERSION /usr/share/$APP/repo/$APP/docker
+if ! docker run --rm $IMAGE pwd >/dev/null 2>&1 ; then
+    echo "Building $IMAGE container."
+    docker build --rm -t $IMAGE /usr/share/$APP/repo/$APP/docker
 fi
 
 docker run \
     -v /etc/localtime:/etc/localtime:ro \
     -v /etc/$APP:/etc/$APP:ro \
     -v /usr/share/$APP/repo:/usr/share/$APP/repo:ro \
-    --name $APP -h $APP $APP:$APP_VERSION \
+    --name $APP -h $APP $IMAGE \
     bash -l /usr/share/$APP/repo/$APP/docker/run.sh
