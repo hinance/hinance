73d49052f35669a9e0d110d4031afda7ee6345b9
Oleg Plakhotniuk <olegus8@gmail.com>
2014-11-14 19:56:37 -0600

    chew

--- hinance-worker/docker/chew.sh
+++ hinance-worker/docker/chew.sh
@@ -4,9 +4,9 @@ set -e
 
 APP="hinance-worker"
 
-mkdir -p /var/lib/$APP/chew.src
-cp -t /var/lib/$APP/chew.src /etc/$APP/*.hs /var/lib/$APP/{bank,shop}_data.hs \
+mkdir -p /tmp/$APP/chew.src
+cp -t /tmp/$APP/chew.src /etc/$APP/*.hs /var/lib/$APP/{bank,shop}_data.hs \
   /usr/share/$APP/repo/$APP/docker/*.hs
-ghc -XFlexibleInstances -o /var/lib/$APP/chew /var/lib/$APP/chew.src/*.hs
+ghc -XFlexibleInstances -o /tmp/$APP/chew /tmp/$APP/chew.src/*.hs
 
-/var/lib/$APP/chew > /var/lib/$APP/chew.hs
+/tmp/$APP/chew > /var/lib/$APP/chew.hs
