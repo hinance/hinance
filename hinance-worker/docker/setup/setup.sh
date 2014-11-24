c93cc8f27d7f051a3efc670e093037f03064c579
Oleg Plakhotniuk <olegus8@gmail.com>
2014-11-24 16:58:28 -0600

    amazon fix

--- hinance-worker/docker/setup/setup.sh
+++ hinance-worker/docker/setup/setup.sh
@@ -72,4 +72,4 @@ paccache -rk0
 mkdir -p /usr/share/$APP/weboob
 git clone git://git.symlink.me/pub/oleg/weboob.git /usr/share/$APP/weboob
 cd /usr/share/$APP/weboob
-git checkout 58066dc
+git checkout 518daa7
