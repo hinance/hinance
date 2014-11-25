ac96f976046a5b21e58db0c47c9fdb0c9dba09cb
Oleg Plakhotniuk <olegus8@gmail.com>
2014-11-25 02:23:16 -0600

    proxy

--- hinance-worker/docker/run.sh
+++ hinance-worker/docker/run.sh
@@ -4,6 +4,19 @@ set -e
 
 APP="hinance-worker"
 
+. /usr/share/$APP/repo/config.sh
+. /etc/$APP/config.sh
+
+if [ "$SOCKS_SSH_HOST" != "" ] ; then
+  echo "Connecting to proxy ${SOCKS_SSH_HOST}."
+  mkdir -p $HOME/.ssh
+  echo "$SOCKS_SSH_HOST" "$SOCKS_SSH_HOST_PUBKEY" > $HOME/.ssh/known_hosts
+  echo "$SOCKS_SSH_USER_PVTKEY" > /var/lib/$APP/socks.pem
+  chmod 600 /var/lib/$APP/socks.pem
+  ssh -fND $SOCKS_PORT -i /var/lib/$APP/socks.pem \
+    ${SOCKS_SSH_USER}@${SOCKS_SSH_HOST}
+fi
+
 ln -s /usr/bin/python{2,}
 export PATH=$PATH:/usr/share/$APP/weboob/scripts
 export PYTHONPATH=$PYTHONPATH:/usr/share/$APP/weboob
