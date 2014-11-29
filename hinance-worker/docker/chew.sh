67bf8a910c689b3954eb01530403e481ff3f4113
Oleg Plakhotniuk <olegus8@gmail.com>
2014-11-29 15:34:14 -0600

    chew faster

--- hinance-worker/docker/chew.sh
+++ hinance-worker/docker/chew.sh
@@ -7,6 +7,6 @@ APP="hinance-worker"
 mkdir -p /tmp/$APP/chew.src
 cp -t /tmp/$APP/chew.src /etc/$APP/*.hs /var/lib/$APP/{bank,shop}_data.hs \
   /usr/share/$APP/repo/$APP/docker/*.hs
-ghc -XFlexibleInstances -o /tmp/$APP/chew /tmp/$APP/chew.src/*.hs
+ghc -O -XFlexibleInstances -o /tmp/$APP/chew /tmp/$APP/chew.src/*.hs
 
 /tmp/$APP/chew > /var/lib/$APP/chew.hs
