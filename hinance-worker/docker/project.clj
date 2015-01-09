ac9786491f0bad91c5f7a44df6ee0a7a38c3fe19
Oleg Plakhotniuk <olegus8@gmail.com>
2015-01-09 00:08:35 -0600

    chew

--- hinance-worker/docker/project.clj
+++ hinance-worker/docker/project.clj
@@ -1,4 +1,4 @@
-(defproject hinance_worker "0.1.0-dev"
+(defproject hinance-worker "0.1.0-dev"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2629"]]
   :plugins [[lein-cljsbuild "1.0.4"]]
