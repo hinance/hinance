fcfaa9f4340baa2d4a6d79bae6504120925f39af
Oleg Plakhotniuk <olegus8@gmail.com>
2015-01-09 00:35:34 -0600

    routes

--- hinance-worker/docker/project.clj
+++ hinance-worker/docker/project.clj
@@ -1,6 +1,7 @@
 (defproject hinance-worker "0.1.0-dev"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2629"]]
+                 [org.clojure/clojurescript "0.0-2629"]
+                 [secretary "1.2.1"]]
   :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild {
     :builds [{
--- hinance-worker/docker/src-cljs/chew.cljs
+++ hinance-worker/docker/src-cljs/chew.cljs
@@ -1,2 +1,42 @@
-(ns chew)
-(js/alert "Hello from Chew!")
+(ns chew
+  (:require [secretary.core :as secretary :refer-macros [defroute]]
+            [goog.events :as events])
+  (:import goog.History
+           goog.history.EventType))
+
+(def application
+  (js/document.getElementById "application"))
+
+(defn set-html! [el content]
+  (aset el "innerHTML" content))
+
+(secretary/set-config! :prefix "#")
+
+;; /#/
+(defroute home-path "/" []
+  (set-html! application "<h1>OMG! YOU'RE HOME!</h1>"))
+
+;; /#/users
+(defroute user-path "/users" []
+  (set-html! application "<h1>USERS!</h1>"))
+
+;; /#/users/:id
+(defroute user-path "/users/:id" [id]
+  (let [message (str "<h1>HELLO USER <small>" id "</small>!</h1>")]
+    (set-html! application message)))
+
+;; /#/777
+(defroute jackpot-path "/777" []
+  (set-html! application "<h1>YOU HIT THE JACKPOT!</h1>"))
+
+;; Catch all
+(defroute "*" []
+  (set-html! application "<h1>LOL! YOU LOST!</h1>"))
+
+;; Quick and dirty history configuration.
+(let [h (History.)]
+  (goog.events/listen h EventType.NAVIGATE #(secretary/dispatch! (.-token %)))
+  (doto h
+    (.setEnabled true)))
+
+(secretary/dispatch! "/")
