07528c8fb3177aff2b4c5ed56af9ad3a29a41863
Oleg Plakhotniuk <olegus8@gmail.com>
2015-01-08 22:15:06 -0600

    reporter

---
+++ hinance-worker/docker/project.clj
@@ -0,0 +1,6 @@
+(defproject reporter "0.1.0-dev"
+  :description "Hinance report browser."
+  :url "https://github.com/olegus8/hinance"
+  :license {:name "The MIT License"
+            :url "https://github.com/olegus8/hinance/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojurescript "0.0-2629"]])
