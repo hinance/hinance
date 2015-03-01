4a47d257f74ee52112f811b122444417812183d4
Oleg Plakhotniuk <olegus8@gmail.com>
2015-02-28 23:05:53 -0600

    fix

--- src/cljs/hinance/main.cljs
+++ src/cljs/hinance/main.cljs
@@ -38,12 +38,12 @@
 
 (defn nav-split [splitn handler params?] (let
   [split (hinance.user/splits splitn) params (or params? {})
-   link (fn [len lim]
-   (vector :a {:href (href :split :split splitn
+   ofs (cljs.reader/read-string (or (params :ofs) "0"))
+   link (fn [len lim] (vector :a {:href (href :split :split splitn
      :step (or (params :step) (step-actual len))
-     :ofs (or (params :ofs) 0) :len (or (params :len) len)
-     :srt (or (params :srt) "time") :asc (or (params :asc) 0)
-     :lim (or (params :lim) lim) :sel-ofs (- len 1) :sel-cat 0)}
+     :ofs ofs :len (or (params :len) len) :srt (or (params :srt) "time")
+     :asc (or (params :asc) 0) :lim (or (params :lim) lim)
+     :sel-ofs (+ ofs (- len 1)) :sel-cat 0)}
      (:title split)))]
   (if (and (= handler :split) (= (str splitn) (params :split)))
     [[:li {:class "active"} [:a (:title split)]]]
