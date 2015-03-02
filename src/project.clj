6c93f3a142cde12657d43c0104ed18c090622468
Oleg Plakhotniuk <olegus8@gmail.com>
2015-03-01 20:05:49 -0600

    new life

--- src/project.clj
+++ src/project.clj
@@ -1,7 +1,6 @@
 (defproject hinance "0.0.0"
   :dependencies [[bidi "1.12.0"]
-                 [com.andrewmcveigh/cljs-time "0.3.0"]
-                 [hiccups "0.3.0"]
+                 [prismatic/dommy "1.0.0"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2665"]]
   :plugins [[lein-cljsbuild "1.0.4"]]
