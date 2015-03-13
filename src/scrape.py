fee2693c0f952061f56733b5102d8e2309f879ba
Oleg Plakhotniuk <olegus8@gmail.com>
2015-03-13 17:11:30 -0500

    allow 0 items

--- src/scrape.py
+++ src/scrape.py
@@ -107,7 +107,6 @@ class MyApp(Application):
       order_total = o.shipping + o.discount + o.tax
       assert payments_total == items_total + order_total, \
         u'%s != %s + %s' % (payments_total, items_total, order_total)
-      assert items_total != 0
 
     print u'Scraped %i orders' % len(sorders)
     stdout.flush()
