6f90d95cef77f1f67f3dd3c6a4616db89c599e7e
Oleg Plakhotniuk <olegus8@gmail.com>
2015-05-05 13:56:04 -0500

    pkg

--- src/hs/webpages.hs
+++ src/hs/webpages.hs
@@ -446,16 +446,12 @@ html titles body =
     "<meta name=\"viewport\" content=\"width=device-width, " ++ 
             "initial-scale=1\">" ++
     "<title>" ++ title ++ "</title>" ++
-    "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com" ++
-                                 "/bootstrap/3.3.1/css/bootstrap.min.css\">"++
-    "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com" ++
-                           "/bootstrap/3.3.1/css/bootstrap-theme.min.css\">"++
+    "<link rel=\"stylesheet\" href=\"bootstrap.min.css\">"++
+    "<link rel=\"stylesheet\" href=\"bootstrap-theme.min.css\">"++
     "</head>" ++
   "<body>" ++ body ++
-    "<script src=\"https://ajax.googleapis.com/ajax/libs" ++ 
-                    "/jquery/1.11.1/jquery.min.js\"></script>" ++
-    "<script src=\"https://maxcdn.bootstrapcdn.com" ++
-                    "/bootstrap/3.3.1/js/bootstrap.min.js\"></script>" ++
+    "<script src=\"jquery.min.js\"></script>" ++
+    "<script src=\"bootstrap.min.js\"></script>" ++
     "<script type=\"text/javascript\" src=\"hinance.js\"></script>" ++
     "</body></html>" where
   title = concat $ intersperse " | " $ titles ++ ["Hinance"]
