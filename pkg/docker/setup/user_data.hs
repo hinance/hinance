5647bcc6b5c5e43d06082fdca0dd1638386bf419
Oleg Plakhotniuk <olegus8@gmail.com>
2015-05-11 21:36:22 -0500

    cards

--- pkg/docker/setup/user_data.hs
+++ pkg/docker/setup/user_data.hs
@@ -31,7 +31,8 @@ instance Patchable Shop where
 
 instance Patchable Bank where
   patched banks = banks ++ [Bank {bid="", baccs=[
-    BankAcc {baid="", balabel="", babalance=300, bacurrency=USD, batrans=[
+    BankAcc {baid="", balabel="", babalance=300, bacurrency=USD, 
+             balimit=Nothing, bapaymin=Nothing, bapaytime=Nothing, batrans=[
       BankTrans {btlabel="", btamount=100, bttime=1},
       BankTrans {btlabel="", btamount=200, bttime=2}]}]}]
 
